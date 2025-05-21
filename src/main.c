d3408eda8aae2beccc295c7e171779f05eef9c2f
borange <dgandersonemail@gmail.com>
2025-05-21 16:11:21 -0500

    Created function parseInput which takes a string of one character and evaluates it against a string of acceptable characters

--- src/main.c
+++ src/main.c
@@ -1,7 +1,41 @@
 #include<stdio.h>
+#include<string.h>
 
-int main () {
-	printf("hello world");
+int parseInput(char input[1]) {
+
+	char acceptableValues[] = {'1', '2', '3'};
+	
+	for (int acceptableValuesIndex = 0; 
+		acceptableValuesIndex < strlen(acceptableValues);
+		acceptableValuesIndex++) {
+
+		if (input[0] != acceptableValues[acceptableValuesIndex]) {
+			printf("Invalid Value Found\n");
+		}
+
+		if (input[0] == acceptableValues[acceptableValuesIndex]) {
+			printf("Valid Value Found\n");
+		}
+	}
+
+	printf("No Invalid Inputs Detected\n");
 
 	return 0;
-	}
+
+}
+
+int main() {
+
+	char input[1];
+
+	printf("Enter Input to Test: ");
+
+	scanf("%s", input);
+
+	printf("\n");
+
+	parseInput(input);
+
+	return 0;
+
+}
